273b320213ad60a3dab21b1a9a353b89259d8a86
user <caioenrique.na@gmail.com>
2024-11-19 15:07:42 -0300

    primeira versão do app de gravação

---
+++ GravadorAudioApp/GravadorAudioApp/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace GravadorAudioApp
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        private void InitializeComponent()
+        {
+            this.labelDevice = new System.Windows.Forms.Label();
+            this.comboBoxDevices = new System.Windows.Forms.ComboBox();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.labelStatus = new System.Windows.Forms.Label();
+            this.labelTimer = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // labelDevice
+            // 
+            this.labelDevice.AutoSize = true;
+            this.labelDevice.Location = new System.Drawing.Point(12, 9);
+            this.labelDevice.Name = "labelDevice";
+            this.labelDevice.Size = new System.Drawing.Size(111, 13);
+            this.labelDevice.TabIndex = 0;
+            this.labelDevice.Text = "Selecionar Dispositivo";
+            // 
+            // comboBoxDevices
+            // 
+            this.comboBoxDevices.FormattingEnabled = true;
+            this.comboBoxDevices.Location = new System.Drawing.Point(12, 25);
+            this.comboBoxDevices.Name = "comboBoxDevices";
+            this.comboBoxDevices.Size = new System.Drawing.Size(270, 21);
+            this.comboBoxDevices.TabIndex = 1;
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(12, 70);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(100, 23);
+            this.buttonStart.TabIndex = 2;
+            this.buttonStart.Text = "Iniciar Gravação";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Location = new System.Drawing.Point(130, 70);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(100, 23);
+            this.buttonStop.TabIndex = 3;
+            this.buttonStop.Text = "Parar Gravação";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+            // 
+            // labelStatus
+            // 
+            this.labelStatus.AutoSize = true;
+            this.labelStatus.Location = new System.Drawing.Point(12, 110);
+            this.labelStatus.Name = "labelStatus";
+            this.labelStatus.Size = new System.Drawing.Size(68, 13);
+            this.labelStatus.TabIndex = 4;
+            this.labelStatus.Text = "Status: Pronto";
+            // 
+            // labelTimer
+            // 
+            this.labelTimer.AutoSize = true;
+            this.labelTimer.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
+            this.labelTimer.Location = new System.Drawing.Point(180, 110);
+            this.labelTimer.Name = "labelTimer";
+            this.labelTimer.Size = new System.Drawing.Size(49, 20);
+            this.labelTimer.TabIndex = 5;
+            this.labelTimer.Text = "00:00";
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(294, 151);
+            this.Controls.Add(this.labelTimer);
+            this.Controls.Add(this.labelStatus);
+            this.Controls.Add(this.buttonStop);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.comboBoxDevices);
+            this.Controls.Add(this.labelDevice);
+            this.Name = "Form1";
+            this.Text = "Gravador de Áudio";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelDevice;
+        private System.Windows.Forms.ComboBox comboBoxDevices;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Button buttonStop;
+        private System.Windows.Forms.Label labelStatus;
+        private System.Windows.Forms.Label labelTimer;
+    }
+}
---
+++ GravadorAudioApp/GravadorAudioApp/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using NAudio.Wave;
+using NAudio.CoreAudioApi;
+
+namespace GravadorAudioApp
+{
+    public partial class Form1 : Form
+    {
+        private WaveInEvent waveIn;
+        private WaveFileWriter writer;
+        private string filePath;
+        private bool isRecording = false;
+        private Timer recordingTimer; // Timer para o tempo de gravação
+        private int recordingSeconds = 0; // Contador do tempo de gravação
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Inicializa o Timer
+            recordingTimer = new Timer
+            {
+                Interval = 1000 // Atualiza a cada 1 segundo
+            };
+            recordingTimer.Tick += RecordingTimer_Tick; // Associa o evento Tick
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadAudioDevices();
+        }
+
+        private void LoadAudioDevices()
+        {
+            var devices = new MMDeviceEnumerator().EnumerateAudioEndPoints(DataFlow.Capture, DeviceState.Active).ToList();
+            foreach (var device in devices)
+            {
+                comboBoxDevices.Items.Add(device.FriendlyName);
+            }
+
+            if (comboBoxDevices.Items.Count > 0)
+            {
+                comboBoxDevices.SelectedIndex = 0;
+            }
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            if (comboBoxDevices.SelectedItem == null)
+            {
+                MessageBox.Show("Selecione um dispositivo de gravação.");
+                return;
+            }
+
+            SaveFileDialog saveFileDialog = new SaveFileDialog
+            {
+                Filter = "Arquivos de Áudio WAV|*.wav",
+                FileName = "gravacao.wav"
+            };
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                filePath = saveFileDialog.FileName;
+                StartRecording();
+            }
+        }
+
+        private void StartRecording()
+        {
+            if (isRecording) return;
+
+            isRecording = true;
+            recordingSeconds = 0; // Reinicia o contador de tempo
+
+            var selectedDevice = comboBoxDevices.SelectedItem.ToString();
+            var enumerator = new MMDeviceEnumerator();
+            var device = enumerator.EnumerateAudioEndPoints(DataFlow.Capture, DeviceState.Active)
+                                   .FirstOrDefault(d => d.FriendlyName == selectedDevice);
+
+            if (device == null)
+            {
+                MessageBox.Show("Dispositivo de gravação não encontrado.");
+                return;
+            }
+
+            waveIn = new WaveInEvent
+            {
+                DeviceNumber = comboBoxDevices.SelectedIndex,
+                WaveFormat = new WaveFormat(44100, 1)
+            };
+
+            waveIn.DataAvailable += OnDataAvailable;
+            waveIn.RecordingStopped += OnRecordingStopped;
+
+            writer = new WaveFileWriter(filePath, waveIn.WaveFormat);
+
+            waveIn.StartRecording();
+
+            recordingTimer.Start(); // Inicia o Timer
+            labelStatus.Text = "Gravando...";
+            labelTimer.Text = "00:00"; // Reseta o tempo
+            buttonStart.Enabled = false;
+            buttonStop.Enabled = true;
+        }
+
+        private void OnDataAvailable(object sender, WaveInEventArgs e)
+        {
+            writer?.Write(e.Buffer, 0, e.BytesRecorded);
+        }
+
+        private void OnRecordingStopped(object sender, StoppedEventArgs e)
+        {
+            writer?.Dispose();
+            waveIn?.Dispose();
+
+            recordingTimer.Stop(); // Para o Timer
+            isRecording = false;
+
+            labelStatus.Text = "Gravação concluída.";
+            labelTimer.Text = "00:00"; // Reseta o contador
+            buttonStart.Enabled = true;
+            buttonStop.Enabled = false;
+        }
+
+        private void RecordingTimer_Tick(object sender, EventArgs e)
+        {
+            recordingSeconds++;
+            int minutes = recordingSeconds / 60;
+            int seconds = recordingSeconds % 60;
+
+            // Atualiza o tempo no Label
+            labelTimer.Text = $"{minutes:D2}:{seconds:D2}";
+        }
+
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+            if (waveIn != null && isRecording)
+            {
+                waveIn.StopRecording();
+            }
+        }
+    }
+}
